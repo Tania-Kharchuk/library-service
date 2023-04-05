ea03b13b8716832333daedba493498e0655d3b26
Tania Kharchuk <tkharchuk14@gmail.com>
2023-04-05 14:05:46 +0300

    Add book model to book_service app and djmoney to settings.py

--- book_service/models.py
+++ book_service/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from djmoney.models.fields import MoneyField
 
-# Create your models here.
+
+class Book(models.Model):
+    class Cover(models.TextChoices):
+        HARD = "HARD"
+        SOFT = "SOFT"
+
+    title = models.CharField(max_length=70)
+    author = models.CharField(max_length=70)
+    cover = models.CharField(max_length=4, choices=Cover.choices)
+    inventory = models.PositiveIntegerField()
+    daily_fee = MoneyField(
+        decimal_places=2,
+        default=0,
+        default_currency="USD",
+        max_digits=11,
+    )
