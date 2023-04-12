0696653dd76a1090dc549ffee25616b544a909e9
Tania Kharchuk <tkharchuk14@gmail.com>
2023-04-12 11:56:35 +0300

    Add borrowing model, make migrations and register it in admin.py

--- borrowing_service/models.py
+++ borrowing_service/models.py
@@ -1,3 +1,20 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+from book_service.models import Book
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateTimeField(auto_now_add=True)
+    expected_return_date = models.DateTimeField()
+    actual_return_date = models.DateTimeField(blank=True, null=True)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowings")
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="borrowings"
+    )
+
+    class Meta:
+        ordering = ["-borrow_date"]
+
+    def __str__(self):
+        return f"{self.user} borrowed {self.book.title}"
