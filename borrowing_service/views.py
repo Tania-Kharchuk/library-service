94750c948e27a35903434e808286d9e8e6192aaf
Tania Kharchuk <tkharchuk14@gmail.com>
2023-04-13 11:43:17 +0300

    Add BorrowingCreateSerializer and modify BorrowingViewSet with creation action

--- borrowing_service/views.py
+++ borrowing_service/views.py
@@ -1,13 +1,26 @@
 from rest_framework import viewsets, mixins
 
 from borrowing_service.models import Borrowing
-from borrowing_service.serializers import BorrowingDetailSerializer
+from borrowing_service.serializers import (
+    BorrowingDetailSerializer,
+    BorrowingCreateSerializer,
+)
 
 
 class BorrowingViewSet(
     mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
     viewsets.GenericViewSet,
 ):
     queryset = Borrowing.objects.select_related("book", "user")
     serializer_class = BorrowingDetailSerializer
+
+    def get_serializer_class(self):
+        if self.action == "create":
+            return BorrowingCreateSerializer
+
+        return BorrowingDetailSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
