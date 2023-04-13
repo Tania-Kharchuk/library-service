183a05ced509bfb7a30ee534711205644875110a
Tania Kharchuk <tkharchuk14@gmail.com>
2023-04-13 13:36:34 +0300

    Add filter by user id and active borrowing to BorrowingViewSet

--- borrowing_service/views.py
+++ borrowing_service/views.py
@@ -1,4 +1,7 @@
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
 from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAuthenticated
 
 from borrowing_service.models import Borrowing
 from borrowing_service.serializers import (
@@ -15,6 +18,24 @@ class BorrowingViewSet(
 ):
     queryset = Borrowing.objects.select_related("book", "user")
     serializer_class = BorrowingDetailSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        is_active = self.request.query_params.get("is_active")
+        user_id = self.request.query_params.get("user_id")
+
+        queryset = self.queryset
+
+        if is_active == "true":
+            queryset = queryset.filter(actual_return_date__isnull=True)
+
+        if user_id and self.request.user.is_staff:
+            queryset = queryset.filter(user_id=int(user_id))
+
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(user=self.request.user)
+
+        return queryset
 
     def get_serializer_class(self):
         if self.action == "create":
@@ -24,3 +45,20 @@ class BorrowingViewSet(
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "is_active",
+                type=OpenApiTypes.BOOL,
+                description="Filter by active borrowings (ex. ?is_active=true)",
+            ),
+            OpenApiParameter(
+                "user_id",
+                type=OpenApiTypes.INT,
+                description="Filter by user_id (ex. ?user_id=2)",
+            ),
+        ]
+    )
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
