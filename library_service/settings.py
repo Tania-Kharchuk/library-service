cac0dfd73e753b6485cd2486cbd5385f67471271
Tania Kharchuk <tkharchuk14@gmail.com>
2023-04-21 11:17:54 +0300

    Delete django-money from installed apps and requirements.txt

--- library_service/settings.py
+++ library_service/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "djmoney",
     "rest_framework",
     "drf_spectacular",
     "book_service",
